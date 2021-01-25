d2892d8408f8ff49a502826dfdfdc212dd8ea8c3
hannes <hannes@gfzdfuz.de>
2021-01-25 21:59:13 +0100

    Future plans.. :)

---
+++ PW/app.svelte
@@ -0,0 +1,11 @@
+<script>
+    import zxcvbn from 'zxcvbn'
+    let password = ''
+    $: security = zxcvbn(password)
+</script>
+  
+<input type=password bind:value={password}>
+
+<p>
+    {security.crack_times_display.offline_fast_hashing_1e10_per_second}
+</p>
