f2c7f77d00797a616d9bdbb6529394e8e9b7a8e5
HannesH <33062605+HannesGitH@users.noreply.github.com>
2022-09-29 23:16:20 +0200

    hm

---
+++ src/routes/icon/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Icon from "$lib/icon.svelte";
+    import { tweened } from 'svelte/motion';
+
+	const progress = tweened(0,{duration:1000});
+</script>
+<Icon animataionprogress={$progress} amplitude={1}/>
+<button on:click="{() => progress.set(0)}">0</button>
+<button on:click="{() => progress.set(.5)}">.5</button>
+<button on:click="{() => progress.set(1)}">1</button>
+<button on:click="{() => progress.set(2)}">2</button>
+<button on:click="{() => progress.set(100)}">100</button>
