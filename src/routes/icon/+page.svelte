9ed2300f2b7f53f63fa40db2c4a5139fc84af37d
HannesH <33062605+HannesGitH@users.noreply.github.com>
2022-09-30 09:39:01 +0200

    kagge mit kite

--- src/routes/icon/+page.svelte
+++ src/routes/icon/+page.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
 	import Icon from '$lib/icon.svelte';
+    import Blob from '$lib/blob.svelte';
 	import { tweened } from 'svelte/motion';
 	export let color = '#c7f347';
 	const progress = tweened(0, { duration: 1000 });
 </script>
 
 <Icon animataionprogress={$progress} amplitude={0.1} asymmetric sideColor={color}/>
+<Blob animataionprogress={$progress} />
 <button on:click={() => progress.set(-0.5)}>-.5</button>
 <button on:click={() => progress.set(0)}>0</button>
 <button on:click={() => progress.set(0.5)}>.5</button>
 <button on:click={() => progress.set(1)}>1</button>
 <button on:click={() => progress.set(2)}>2</button>
 <button on:click={() => progress.set(100)}>100</button>
+
+<style>
+    :global body {
+        background-color: #000;
+    }
+</style>
