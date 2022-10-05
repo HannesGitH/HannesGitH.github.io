6a3a9a7683ac3c78867ab493e9a9cc9bc2dabebb
HannesH <33062605+HannesGitH@users.noreply.github.com>
2022-10-05 18:32:50 +0200

    katyes yes yes yes yes

---
+++ src/lib/sections/refs.ts
@@ -0,0 +1,8 @@
+export default {
+	me: 'section:me',
+	education: 'section:education',
+	experience: 'section:experience',
+	skills: 'section:skills',
+	projects: 'section:projects',
+	contact: 'section:contact'
+};
