c8c03cb74538aa066604edebcd1257964b21036a
Hannes <33062605+HannesGitH@users.noreply.github.com>
2024-04-01 19:59:57 +0200

    can build with navbar

--- flake.nix
+++ flake.nix
@@ -15,6 +15,34 @@
       nodejs = pkgs.nodejs;
       esbuild = pkgs-stable.esbuild;
 
+      svelte-navbar-src = pkgs.fetchFromGitHub {
+        owner = "hannesgith";
+        repo = "svelte-navbar";
+        rev = "0.0.123";
+        sha256 = "sha256-naxceQenpoSbjlYLaLlYxJElddk0qplXp7K+4KIzA8M=";
+      };
+
+      svelte-navbar = pkgs.mkYarnPackage {
+        name = "svelte-navbar";
+        src = svelte-navbar-src;
+        buildInputs = with pkgs; [ nodejs git ];
+        configurePhase = ''
+          cp -r $node_modules node_modules
+          chmod -R +w node_modules
+          ln -sf ${esbuild}/bin/esbuild node_modules/esbuild/bin/esbuild
+          ln -sf ${esbuild}/bin/esbuild node_modules/esbuild-linux-64/bin/esbuild
+        '';
+        buildPhase = ''
+          # yarn install --offline --frozen-lockfile
+          yarn --offline --frozen-lockfile build
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp -r . $out
+        '';
+        distPhase = "true";
+      };
+
       # node2nixOutput = import ./default.nix { inherit pkgs nodejs system; };
       # nodeDeps = node2nixOutput.nodeDependencies;
 
@@ -22,22 +50,29 @@
       packages = rec {
         website = pkgs.mkYarnPackage {
           name = "website";
+          
           src = self;
           buildInputs = with pkgs; [ nodejs git ];
           configurePhase = ''
-            for localDir in build node_modules; do
-              if [[ -d $localDir || -L $localDir ]]; then
-                echo "$localDir dir present. Removing."
-                rm -rf $localDir
-              fi
-            done
+            # for localDir in build node_modules; do
+            #   if [[ -d $localDir || -L $localDir ]]; then
+            #     echo "$localDir dir present. Removing."
+            #     rm -rf $localDir
+            #   fi
+            # done
+
+            ln -sf ${svelte-navbar} dependencies/svelte-navbar
 
             cp -r $node_modules node_modules
             chmod -R +w node_modules
+            # ls -la src/lib
+            # ls -la dependencies
+            # ls -la .
             ln -sf ${esbuild}/bin/esbuild node_modules/esbuild/bin/esbuild
             ln -sf ${esbuild}/bin/esbuild node_modules/esbuild-linux-64/bin/esbuild
           '';
           buildPhase = ''
+            # yarn install --offline --frozen-lockfile
             yarn --offline --frozen-lockfile build
           '';
           installPhase = ''
@@ -50,6 +85,9 @@
 
       shell = pkgs.mkShell {
         buildInputs = with pkgs; [ nodejs git ];
+        shellHook = ''
+          # yarn install
+        '';
       };
 
       defaultPackage = packages.website;
