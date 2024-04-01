c5209fce42c33398386faa41662c9ddd1aea78ec
Hannes <33062605+HannesGitH@users.noreply.github.com>
2024-04-01 18:30:44 +0200

    node2nix

--- flake.nix
+++ flake.nix
@@ -5,13 +5,16 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
   };
-  
+
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
     with flake-utils.lib; eachSystem allSystems (system:
     let
       pkgs = import nixpkgs { inherit system; };
       pkgs-stable = import inputs.nixpkgs-stable { inherit system; };
-      
+
+      node2nixOutput = import ./default.nix { inherit pkgs nodejs system; };
+      nodeDeps = node2nixOutput.nodeDependencies;
+
     in rec {
       packages = rec {
         website = pkgs.stdenvNoCC.mkDerivation {
@@ -19,16 +22,18 @@
           src = self;
           buildInputs = with pkgs; [ nodejs git ];
           buildPhase = ''
-            git submodule update --init
-            cd dependencies/svelte-navbar && npm install && npm run build && cd ../..
-            mkdir -p out
-            npm install
+          # export PATH="$PWD/node_modules/.bin/:$PATH"
+          #   git submodule update --init
+          #   cd dependencies/svelte-navbar && npm install && npm run build && cd ../..
+            # npm install
+            ln -sf ${nodeDeps}/lib/node_modules ./node_modules
+            
             npm run build
           '';
           installPhase = "cp -r build $out";
         };
       };
-      
+
       shell = pkgs.mkShell {
         buildInputs = with pkgs; [ nodejs git ];
       };
