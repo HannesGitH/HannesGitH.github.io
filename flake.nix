7b9ba28428dffaa1914b830a709e837fa2486fe9
Hannes <33062605+HannesGitH@users.noreply.github.com>
2024-04-01 17:59:58 +0200

    deps

--- flake.nix
+++ flake.nix
@@ -1,15 +1,38 @@
 {
-  description = "A very basic flake";
-
+  description = "CV website";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
   };
+  
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    with flake-utils.lib; eachSystem allSystems (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      pkgs-stable = import inputs.nixpkgs-stable { inherit system; };
+      
+    in rec {
+      packages = rec {
+        website = pkgs.stdenvNoCC.mkDerivation {
+          name = "website";
+          src = self;
+          buildInputs = with pkgs; [ nodejs git ];
+          buildPhase = ''
+            git submodule update --init
+            cd dependencies/svelte-navbar && npm install && npm run build && cd ../..
+            mkdir -p out
+            npm install
+            npm run build
+          '';
+          installPhase = "cp -r build $out";
+        };
+      };
+      
+      shell = pkgs.mkShell {
+        buildInputs = with pkgs; [ nodejs git ];
+      };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+      defaultPackage = packages.website;
+    });
 }
